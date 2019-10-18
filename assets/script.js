d819528725a16ad6e1ffd92e1c083882e6a896b1
Sheng-Liang Slogar <sheng@diglactic.com>
2019-10-18 09:09:52 -0600

    Add JavaScript to prevent transition flashes :rolling_eyes:

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
-{
-  // ga --
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-135017895-1');
-}
+// ga
+window.dataLayer = window.dataLayer || [];
+function gtag() {dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-135017895-1');
+
+// prevent transitions at load
+// @see https://stackoverflow.com/a/42969608/2535504
+window.addEventListener('load', () => {
+  document.body.classList.remove('preload');
+});
