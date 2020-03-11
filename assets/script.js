f2ab63f7344c5fd1263880ca374bc0a3590a100a
Sheng-Liang Slogar <sheng@diglactic.com>
2020-03-10 21:03:30 -0600

    Begin v2

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,21 @@
-// ga
+/**
+ * google analytics
+ */
+
 window.dataLayer = window.dataLayer || [];
-function gtag() {dataLayer.push(arguments);}
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
 gtag('js', new Date());
 gtag('config', 'UA-135017895-1');
 
-// prevent transitions at load
-// @see https://stackoverflow.com/a/42969608/2535504
+/**
+ * prevent transitions at load
+ * @see https://stackoverflow.com/a/42969608/2535504
+ */
+
 window.addEventListener('load', () => {
-  document.body.classList.remove('preload');
+    document.body.classList.remove('preload');
 });
