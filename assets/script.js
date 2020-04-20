574f55ed2ad4eb3bebc3230f63028c9d279585e3
Sheng-Liang Slogar <sheng@diglactic.com>
2020-04-19 19:37:53 -0600

    mvp

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,13 @@
  * google analytics
  */
 
+var gid = 'UA-135017895-1';
+
+var gscript = document.createElement('script');
+gscript.async = true;
+gscript.src = 'https://www.googletagmanager.com/gtag/js?id=' + gid;
+document.body.appendChild(gscript);
+
 window.dataLayer = window.dataLayer || [];
 
 function gtag() {
@@ -9,13 +16,26 @@ function gtag() {
 }
 
 gtag('js', new Date());
-gtag('config', 'UA-135017895-1');
+gtag('config', gid);
+
+/**
+ * lazy load images
+ */
+document.querySelectorAll('img')
+    .forEach(function (image) {
+        if (!image.complete) {
+            image.addEventListener('load', function () {
+                image.classList.add('lazy--loaded');
+            });
+            image.classList.add('lazy');
+        }
+    });
 
 /**
  * prevent transitions at load
  * @see https://stackoverflow.com/a/42969608/2535504
  */
 
-window.addEventListener('load', () => {
+window.addEventListener('load', function () {
     document.body.classList.remove('preload');
 });
